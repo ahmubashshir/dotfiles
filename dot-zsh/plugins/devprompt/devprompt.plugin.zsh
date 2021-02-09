e57fcbee5446b9c8eb645c028abb9d857be65a8f
Mubashshir <ahmubashshir@gmail.com>
2021-02-09 18:51:01 +0600

    update devprompt plugin

--- dot-zsh/plugins/devprompt/devprompt.plugin.zsh
+++ dot-zsh/plugins/devprompt/devprompt.plugin.zsh
@@ -3,19 +3,19 @@
 setopt PROMPT_SUBST
 autoload -U add-zsh-hook
 
-typeset -gA DEVPROMPT_CONFIG
+typeset -gA DEVPROMPT
 
-DEVPROMPT_CONFIG[prefix]='['
-DEVPROMPT_CONFIG[suffix]=']'
-DEVPROMPT_CONFIG[seperator]='-'
-DEVPROMPT_CONFIG[icon]='%F{blue}'
-DEVPROMPT_CONFIG[text]='%F{green}'
-DEVPROMPT_CONFIG[default]='%F{red}'
+DEVPROMPT[prefix]='['
+DEVPROMPT[suffix]=']'
+DEVPROMPT[seperator]='-'
+DEVPROMPT[icon]='%F{blue}'
+DEVPROMPT[text]='%F{green}'
+DEVPROMPT[default]='%F{red}'
 
 add-zsh-hook precmd __DEVPROMPT_precmd
 
 function __DEVPROMPT_precmd {
 	DEVPROMPT_PROMPT=''
 	[[ -n $VIRTUAL_ENV ]] && \
-		DEVPROMPT_PROMPT+="${DEVPROMPT_CONFIG[seperator]}${DEVPROMPT_CONFIG[prefix]}${DEVPROMPT_CONFIG[icon]}%B ${DEVPROMPT_CONFIG[text]}${VIRTUAL_ENV##*/}%b${DEVPROMPT_CONFIG[default]}${DEVPROMPT_CONFIG[suffix]}"
+		DEVPROMPT_PROMPT+="${DEVPROMPT[seperator]}${DEVPROMPT[prefix]}${DEVPROMPT[icon]}%B ${DEVPROMPT[text]}${VIRTUAL_ENV##*/}%b${DEVPROMPT[default]}${DEVPROMPT[suffix]}"
 }
