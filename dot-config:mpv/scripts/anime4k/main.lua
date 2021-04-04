868b8fcac8a8c355a4f7595b2d6aa478c23c70c8
Mubashshir <ahmubashshir@gmail.com>
2021-04-04 20:02:29 +0600

    update anime4k scripts

--- dot-config:mpv/scripts/anime4k/main.lua
+++ dot-config:mpv/scripts/anime4k/main.lua
@@ -2,6 +2,18 @@ local utils = require "mp.utils"
 local msg = require "mp.msg"
 local options = require "mp.options"
 local shaders = require "shaders"
+local o = {
+    enable = false,
+    mode = 'faithful',
+    uhd = false,
+    force = false
+    -- Modes:
+    -- faithful: Upscale video while being faithful to the original video
+    -- deblur: Improve perceptual quality and deblur
+    -- perceptual: Improve perceptual quality.
+}
+
+options.read_options(o, 'anime4k')
 
 local shader_path = mp.find_config_file("shaders")
 
@@ -11,7 +23,7 @@ local listsep = not pathsep == '/' and ';' or ':'
 
 local function load_shaders(...)
     local list = {}
-
+    msg.info("Mode: " .. o.mode)
     for _, shader in ipairs(...) do
         if shader == nil then goto continue end
         table.insert(list, table.concat({
@@ -27,14 +39,23 @@ end
 local function load_shader()
     height = mp.get_property_native("height")
     msg.info("Res: " .. height .. "p")
-    if height > 720 then
-        load_shaders(shaders["hd_faithful"])
-    elseif height <= 720 and height >= 480 then
-        load_shaders(shaders["default_faithful"])
+    if height > 720 and o.uhd then
+        load_shaders(shaders["hd_" .. o.mode])
+    elseif height <= 720 and (height >= 480 or o.force) then
+        load_shaders(shaders["default_" .. o.mode])
     else
         load_shaders(shaders["none"])
     end
 end
+if (function(tab, val)
 
-mp.register_event("file-loaded", load_shader)
+    for index, value in ipairs(tab) do if value == val then return false end end
+    return true
+end)({'faithful', 'perceptual', 'deblur'}, o.mode) then
 
+    msg.warn('Invalid mode: ' .. o.mode)
+    o.mode = 'faithful'
+    msg.info('Fallback mode: ' .. o.mode)
+end
+
+if o.enable then mp.register_event("file-loaded", load_shader) end
--- dot-config:mpv/scripts/anime4k/shaders.lua
+++ dot-config:mpv/scripts/anime4k/shaders.lua
@@ -19,12 +19,12 @@ shaders["hd_perceptual"] = {
     "Upscale_CNN_M_x2_Deblur"
 }
 -- Anime4K: 480/720p (Perceptual Quality and Deblur)
-shaders["default_perceptual_deblur"] = {
+shaders["default_deblur"] = {
     "Upscale_CNN_L_x2_Denoise", "Auto_Downscale_Pre_x4", "Deblur_DoG",
     "DarkLines_HQ", "ThinLines_HQ", "Upscale_CNN_M_x2_Deblur"
 }
 -- Anime4K: 1080p (Perceptual Quality and Deblur)
-shaders["hd_perceptual_deblur"] = {
+shaders["hd_deblur"] = {
     "Denoise_Bilateral_Mode", "Deblur_DoG", "DarkLines_HQ", "ThinLines_HQ",
     "Upscale_CNN_M_x2_Deblur"
 }
