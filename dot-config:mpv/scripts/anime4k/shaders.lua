83ae4e28b60fa208ca65a4d39cdca467f5ba80b3
Mubashshir <ahmubashshir@gmail.com>
2021-04-04 15:58:07 +0600

    add anime4k script mpv

---
+++ dot-config:mpv/scripts/anime4k/shaders.lua
@@ -0,0 +1,32 @@
+local shaders = {}
+
+-- Anime4K: unset
+shaders["none"] = {}
+-- Anime4K: 480/720p (Faithful)
+shaders["default_faithful"] = {
+    "Upscale_CNN_L_x2_Denoise", "Auto_Downscale_Pre_x4", "Auto_Downscale_Pre_x4"
+}
+-- Anime4K: 1080p (Faithful)
+shaders["hd_faithful"] = {"Denoise_Bilateral_Mode", "Upscale_CNN_M_x2_Deblur"}
+-- Anime4K: 480/720p (Perceptual Quality)
+shaders["default_perceptual"] = {
+    "Upscale_CNN_L_x2_Denoise", "Auto_Downscale_Pre_x4", "DarkLines_HQ",
+    "ThinLines_HQ", "Upscale_CNN_M_x2_Deblur"
+}
+-- Anime4K: 1080p (Perceptual Quality)
+shaders["hd_perceptual"] = {
+    "Denoise_Bilateral_Mode", "DarkLines_HQ", "ThinLines_HQ",
+    "Upscale_CNN_M_x2_Deblur"
+}
+-- Anime4K: 480/720p (Perceptual Quality and Deblur)
+shaders["default_perceptual_deblur"] = {
+    "Upscale_CNN_L_x2_Denoise", "Auto_Downscale_Pre_x4", "Deblur_DoG",
+    "DarkLines_HQ", "ThinLines_HQ", "Upscale_CNN_M_x2_Deblur"
+}
+-- Anime4K: 1080p (Perceptual Quality and Deblur)
+shaders["hd_perceptual_deblur"] = {
+    "Denoise_Bilateral_Mode", "Deblur_DoG", "DarkLines_HQ", "ThinLines_HQ",
+    "Upscale_CNN_M_x2_Deblur"
+}
+
+return shaders
