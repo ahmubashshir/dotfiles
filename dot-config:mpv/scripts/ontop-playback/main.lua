f8b671f42ed6e9b37ae0a9fe7139851179169113
Mubashshir <ahmubashshir@gmail.com>
2021-04-10 17:42:08 +0600

    update mpv-ontop lua script

--- dot-config:mpv/scripts/ontop-playback/main.lua
+++ dot-config:mpv/scripts/ontop-playback/main.lua
@@ -1,6 +1,8 @@
 -- makes mpv disable ontop when pausing and re-enable it again when resuming playback
 if mp.get_property_native("ontop") then
     mp.observe_property("pause", "bool", function(name, value)
-        mp.set_property_native("ontop", not value)
+
+        mp.set_property_native("ontop", mp.get_property_native("fullscreen") or
+                                   not value)
     end)
 end
