1e6170dda80b2d965ba5155351dd9add5672d195
Mubashshir <ahmubashshir@gmail.com>
2021-04-24 00:51:39 +0600

    Update bin/is.gd

--- bin/is.gd
+++ bin/is.gd
@@ -3,13 +3,14 @@
 ( ($# < 2)) && exit 1
 url=$(curl -s "https://is.gd/create.php?format=simple&url=$2")
 if echo "$url" | grep -q '://is\.gd/'; then
-	echo "$url" | case $1 in
+	echo -n "$url" | case $1 in
 		copy)
 			xclip -selection clipboard
 			notify-send -a is.gd -i link "LinkShrink" "Shortened to: $url"
 			;;
 		*)
 			cat
+			echo
 			;;
 	esac
 fi
