66f4efaecd15e61ab1b9cc7249eadb0e613b9039
Mubashshir <ahmubashshir@gmail.com>
2021-04-24 00:00:44 +0600

    Add url shortener

---
+++ bin/is.gd
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+( ($# < 2)) && exit 1
+url=$(curl -s "https://is.gd/create.php?format=simple&url=$2")
+if echo "$url" | grep -q '://is\.gd/'; then
+	echo "$url" | case $1 in
+		copy)
+			xclip -selection clipboard
+			notify-send -a is.gd -i link "LinkShrink" "Shortened to: $url"
+			;;
+		*)
+			cat
+			;;
+	esac
+fi
+# vim: ts=4:ft=sh:et
