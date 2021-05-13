67ac5aaf4e41a2aa6a880bc85853e07e868cedb6
Mubashshir <ahmubashshir@gmail.com>
2021-05-13 18:08:01 +0600

    [mpv] Use native commands in auto-save-state

--- dot-config:mpv/scripts/auto-save-state/main.lua
+++ dot-config:mpv/scripts/auto-save-state/main.lua
@@ -1,16 +1,18 @@
 -- Runs write-watch-later-config periodically
 local utils = require 'mp.utils'
 local options = require 'mp.options'
-local md5 = require 'md5'
+local msg = require 'mp.msg'
 
 local o = {save_interval = 60}
 options.read_options(o)
 
-local function save()
-    mp.commandv("set", "msg-level", "cplayer=warn")
-    mp.command("write-watch-later-config")
-    -- FIXME this overwrites msg-level=cplayer=? original value
-    mp.commandv("set", "msg-level", "cplayer=status")
+local function async_cb(...) return true end
+
+local function save(...)
+    remaining = mp.get_property_native("playtime-remaining")
+    if remaining and remaining > 10 then
+        mp.command_native_async({"write-watch-later-config"}, async_cb)
+    end
 end
 
 local function save_if_pause(_, pause) if pause then save() end end
@@ -32,44 +34,15 @@ end
 -- This function is called on file-loaded instead of end-file because
 -- the next file in the playlist would likely be loaded by the time the
 -- end-file event runs.
-local function clean_watch_later(event)
-    -- The hashing used to generate the watch_later entries needs
-    -- to be replicated here since mpv doesn't provide an API for this. This
-    -- should work with GNU coreutils and busybox, I haven't tested with
-    -- anything else.
-    local path = mp.get_property("path")
-    local cwd = utils.getcwd()
-    if path == nil or cwd == nil then do return end end
-
-    local abs_path = utils.join_path(cwd, path)
-    local hash = md5.sumhexa(abs_path):upper()
-
-    local watch_later = mp.find_config_file("watch_later")
-    if hash == nil or watch_later == nil then do return end end
-
-    -- Here we finally get the path to the watch_later entry
-    local hashfile = utils.join_path(watch_later, hash)
-
-    -- We use currying to pass the watch_later entry to the callback
-    function rm_hashfile(hashfile)
-        return function(event)
-            mp.unregister_event(rm_hashfile(hashfile))
-            if event["reason"] == "eof" or event["reason"] == "stop" then
-                os.remove(hashfile)
-            end
-        end
+local function delete(_, remaining)
+    if remaining and remaining < 10 then
+        mp.command_native_async({"delete-watch-later-config"}, async_cb)
     end
-    mp.register_event("end-file", rm_hashfile(hashfile))
 end
 
 timer = mp.add_periodic_timer(o.save_interval, save)
 mp.observe_property("pause", "bool", pause_timer_while_paused)
 mp.observe_property("pause", "bool", save_if_pause)
 
--- save is called prior to clean_watch_later to ensure the watch_later
--- directory is created first, otherwise clean_watch_later would fail
-mp.register_event("file-loaded", function()
-    save();
-    clean_watch_later()
-end)
-
+mp.observe_property("playtime-remaining", "native", delete)
+mp.register_event("file-loaded", save)
