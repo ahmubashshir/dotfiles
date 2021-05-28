590c1ce3f09d31300d604309e6ed2f4e5c7c308b
Mubashshir <ahmubashshir@gmail.com>
2021-05-28 16:48:42 +0600

    [firefox: user.js] update

--- etc/firefox/user.js
+++ etc/firefox/user.js
@@ -1,3 +1,6 @@
 user_pref( "toolkit.legacyUserProfileCustomizations.stylesheets", true ); // default is false
 user_pref( "svg.context-properties.content.enabled", true );
 user_pref( "browser.tabs.allowTabDetach", false );
+user_pref( "network.captive-portal-service.enabled", false );
+user_pref( "browser.selfsupport.url", "" );
+user_pref( "pocket.enabled", false );
