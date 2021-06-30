5427898b2b30ae786fb2e40887092ea70038e02c
Mubashshir <ahmubashshir@gmail.com>
2021-07-01 04:43:10 +0600

    Update trackma private keys helper

--- dot-config:trackma/hooks/private/__init__.py
+++ dot-config:trackma/hooks/private/__init__.py
@@ -1,4 +1,22 @@
-try:
-    from .twitter import TwitterAccess
-except ImportError:
-    pass
+"""
+This package holds private credentials.
+"""
+
+from enum import Enum, auto
+
+
+class Creds(Enum):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        attribs = dir(self.value)
+        for each in dir(Enum):
+            if each in attribs:
+                attribs.remove(each)
+        for each in attribs:
+            setattr(self, each, getattr(self.value, each))
+
+    def __str__(self):
+        if isinstance(self.value, (str,)):
+            return self.value
+        else:
+            return self.name.replace('_', ' ')
