1384b601195b5174fa356720b6227c4554780dca
Mubashshir <ahmubashshir@gmail.com>
2021-09-18 14:11:10 +0600

    zsh/plugins/notify: Optimize code

--- dot-config:zsh/plugins/notify/notify.plugin.zsh
+++ dot-config:zsh/plugins/notify/notify.plugin.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # commands to ignore
+zmodload -F zsh/datetime p:EPOCHSECONDS
 typeset -gA NOTIF
 NOTIF[tmout]=${NOTIF[tmout]:-10}
 NOTIF[units]=${NOTIF[units]:-0}
@@ -12,26 +13,26 @@ add-zsh-hook precmd notifyosd_precmd
 # end and compare timer, notify-send if needed
 function notifyosd_precmd() {
     retval=$?
-    unset_vars()
-    {
-        unset NOTIF[cmd] NOTIF[basename] NOTIF[start]
-        unfunction unset_vars
-    }
+    vars=(
+        'NOTIF[cmd]'
+        'NOTIF[basename]'
+        'NOTIF[start]'
+    )
     ${ENV[gnome_vfs]:-false} && {
-        unset_vars
+        unset $vars
         return 0
     }
     local -A msg dat
 	if [[ ${cmdignore[(r)$NOTIF[basename]]} == $NOTIF[basename] ]]; then
-        unset_vars
+        unset $vars
         return
     else
         if [ ! -z "$NOTIF[cmd]" ]; then
-            dat[end]=`date +%s`
+            dat[end]=$EPOCHSECONDS
             ((dat[secs]=$dat[end] - $NOTIF[start]))
         fi
         if [ $retval -gt 0 ]; then
-			dat[cmd]="with warning"
+			dat[cmd]="with return $retval"
 			dat[snd]="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
 			msg[u]=2
 		else
@@ -40,7 +41,7 @@ function notifyosd_precmd() {
 			msg[u]=1
         fi
         if [ ! -z "$NOTIF[cmd]" -a $dat[secs] -gt ${NOTIF[tmout]:-10} ]; then
-			if [ $NOTIF[units] -gt 0 ] && [ -x /usr/bin/units ]; then
+			if [ $NOTIF[units] -gt 0 ] && whence -p units > /dev/null; then
 				dat[time]=$(units "$dat[secs] seconds" "centuries;years;months;weeks;days;hours;minutes;seconds" | \
 						sed -e 's/\ +/\,/g' -e s'/\t//')
 			else
@@ -55,31 +56,63 @@ function notifyosd_precmd() {
 
             fi
             NOTIF[id]=$(
-                gdbus call --session \
-                --dest org.freedesktop.Notifications \
-                --object-path /org/freedesktop/Notifications \
-                --method org.freedesktop.Notifications.Notify \
-                "zsh@$$" \
-                "${NOTIF[id]:-0}" \
-                utilities-terminal \
-                "$msg[s]" \
-                "$msg[b]" \
-                '[]' \
-                "{'urgency': <byte $msg[u]>}" \
-                15000 \
-                | sed -E 's/\(uint32 ([[:digit:]]+),.*/\1/g'
+                notifyosd_send "$msg[s]" "$msg[b]" "$msg[u]"
             )
             play -q $dat[snd] 2>/dev/null
         fi
     fi
-    unset_vars
+    unset $vars
 }
 
+# Send notification
+function notifyosd_send() {
+    local app bus dest name
+    bus="org.freedesktop.Notifications"
+    dest=/org/freedesktop/Notifications
+    name=Notify
+
+    () {
+        local each
+        for each in "$@";do
+            whence -p $each > /dev/null || continue
+            app=$each
+            return
+        done
+    } busctl gdbus notify-send
+    ! {
+        [[ -n $DBUS_SESSION_BUS_ADDRESS ]] \
+        && dbus-send --print-reply --dest=org.freedesktop.DBus \
+            /org/freedesktop/DBus org.freedesktop.DBus.ListNames \
+            | grep -q 'org\.freedesktop\.Notifications'
+    } && return 0
+    case $app in
+        (busctl)
+            busctl --user \
+                call "$bus" "$dest" "$bus" \
+                "$name" 'susssasa{sv}i' \
+                "zsh@$$" "${NOTIF[id]:-0}" utilities-terminal "$1" "$2" \
+                0 1 urgency y "$3" 15000 \
+             | sed -E 's/^u ([[:digit:]]+)$/\1/g'
+        ;;
+        (gdbus)
+            gdbus call --session --dest "$bus" \
+                --object-path "$dest" --method "$bus.$name" \
+                "zsh@$$" "${NOTIF[id]:-0}" utilities-terminal "$1" "$2" \
+                '[]' "{'urgency': <byte $3>}" 15000 \
+             | sed -E 's/\(uint32 ([[:digit:]]+),.*/\1/g'
+        (notify-send)
+            notify-send -t 15000 -a "zsh@$$" \
+                -i utilities-terminal \
+                -h byte:urgency:$3 \
+                "$1" "$2"
+        ;;
+    esac
+}
 
 # get command name and start the timer
 function notifyosd_preexec() {
 	${ENV[gnome_vfs]:-false} && return 0
     NOTIF[cmd]=$1
     NOTIF[basename]=${${NOTIF[cmd]:s/sudo //}[(ws: :)1]}
-    NOTIF[start]=`date +%s`
+    NOTIF[start]=$EPOCHSECONDS
 }
