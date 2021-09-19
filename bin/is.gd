820d50b6dff27c8c5074777f85c8e70617507829
Mubashshir <ahmubashshir@gmail.com>
2021-09-20 00:20:05 +0600

    bin/is.gd: Add help text

--- bin/is.gd
+++ bin/is.gd
@@ -1,7 +1,11 @@
 #!/bin/sh
-
-( ($# < 2)) && exit 1
-url=$(curl -s "https://is.gd/create.php?format=simple&url=$2")
+if test $# -lt 2; then
+	printf '%s\n' \
+		"is.gd usage:" \
+		"  is.gd <copy|cat> <link>"
+	exit
+fi
+url=$(curl --disable -s "https://is.gd/create.php?format=simple&url=$2")
 if echo "$url" | grep -q '://is\.gd/'; then
 	echo -n "$url" | case $1 in
 		copy)
