f03ca29de173b993add0f8e29f445361ab42a17e
Mubashshir <ahmubashshir@gmail.com>
2021-10-29 05:29:51 +0600

    mpv: Update snaps/record organizer

--- Videos:records:org.zsh
+++ Videos:records:org.zsh
@@ -2,20 +2,20 @@
 cd "${0:h}"
 setopt nullglob extendedglob rematchpcre nocaseglob hist_subst_pattern
 
-for each in **/*(ep|ova)-*.(webp|png);do
+for each in **/*(ep|ova)-*.mp4;do
 	meta=${each:s+/+-+:gs/:/∶/}
 	meta=${each:s%-(#bi)(((ep(isode|)|ova)(-*|))-?ubbed)-%/${match[1]:l}/%}
 	name=${meta:h1}
 	meta=${meta:t2}
-	echo :: $name, ${meta:h}, ${meta:h2:t:r:gs+.+∶+}, ${meta:e}
+	echo :: $name, ${meta:h}, ${meta:h2:t:r}, ${meta:e}
 	mkdir -p "$name/${meta:h}"
 	mv "$each" "$name/${meta:h}/${meta:h2:t:r}.${meta:e}"
 done
 
-for each in **/*-*-*.mp4;do
+for each in **/*-*-*-*.mp4;do
 	count=0
 	meta=${each:s+/+-+:gs/:/∶/}
-	meta=${each:s+(#bi)(*)-(*)-(*).mp4+'${match[1]}/${match[2]}/${match[3]}.mp4'+}
+	meta=${each:s+(#bi)(*)-(*-*)-(*).mp4+'${match[1]}/${match[2]}/${match[3]}.mp4'+}
 	echo :: ${meta:h1}, ${meta:h2:t:r}, ${meta:e}
 	mkdir -p "${meta:h1}"
 	mv "$each" "${meta:h1}/${meta:h2:t:r}.${meta:e}"
