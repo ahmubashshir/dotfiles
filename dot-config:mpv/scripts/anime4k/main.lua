56fdd156c9748b89f183c832db0faf9765c38eaf
Mubashshir <ahmubashshir@gmail.com>
2021-11-03 22:07:43 +0600

    mpv/anime4k: Add override by property
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- dot-config:mpv/scripts/anime4k/main.lua
+++ dot-config:mpv/scripts/anime4k/main.lua
@@ -2,9 +2,11 @@ local utils = require "mp.utils"
 local msg = require "mp.msg"
 local options = require "mp.options"
 local shaders = require "shaders"
+local csv = require "csv"
 
 local o = {
-    enable = false,
+    enable = true,
+    autoload = false,
     uhd = false,
     hd = false,
     quality = 'fast', -- Quality: If upscaling to resolutions smaller than 4K, lower end GPUs can be used.
@@ -19,7 +21,7 @@ local o = {
     -- BB: Restore_Soft -> Upscale -> Restore_Soft -> Upscale
     -- CA: Upscale_Denoise -> Restore -> Upscale
 }
-local valid_modes = {'a', 'b', 'c', 'aa', 'bb', 'ca'}
+local valid_modes = {'a', 'b', 'c', 'aa', 'bb', 'ca', 'none'}
 options.read_options(o, 'anime4k')
 
 local shader_path = mp.find_config_file("shaders")
@@ -28,7 +30,7 @@ local pathsep = package.config:sub(1, 1)
 local listsep = not pathsep == '/' and ';' or ':'
 
 -- no-osd change-list glsl-shaders set
-local function async_cb(...) msg.info(...) end
+local function async_cb(...) return nil end
 local function validate_mode(val)
     for index, value in ipairs(valid_modes) do
         if value == val then return true end
@@ -45,26 +47,52 @@ local function append_list(list, src)
                 }, pathsep))
             msg.info('Loaded Shaders: ' .. shaders.resolve(shader))
         end
-        ::continue::
     end
 end
 
 local function load_shaders(mode)
-    local list = {}
-    msg.info("Mode: " .. o.mode)
-    mp.osd_message("[" .. mp.get_script_name() .. "] Mode: " .. o.mode)
-    append_list(list, shaders["common"])
-    append_list(list, shaders[o.quality][o.mode])
-    mp.command_native_async({
-        "change-list", "glsl-shaders", "set", table.concat(list, listsep)
-    }, async_cb)
+    if not validate_mode(mode) then return nil end
+    msg.info("Mode: " .. mode)
+    mp.osd_message("[" .. mp.get_script_name() .. "] Mode: " .. mode)
+    mp.command_native_async({"change-list", "glsl-shaders", "set", ""}, async_cb)
+
+    if not (mode == 'none') then
+        local list = {}
+        append_list(list, shaders["common"])
+        append_list(list, shaders[o.quality][mode])
+        mp.command_native_async({
+            "change-list", "glsl-shaders", "set", table.concat(list, listsep)
+        }, async_cb)
+    end
 end
 
-local function unload_shaders()
-    msg.info("Mode: none")
-    mp.osd_message("[" .. mp.get_script_name() .. "] Mode: none")
-    mp.command_native_async({"change-list", "glsl-shaders", "set", ""}, async_cb)
-    -- pass
+local function match_prop(name, match)
+    local val = mp.get_property(name)
+    if val == nil then return false end
+    if string.match(val, match) == nil then return false end
+    return true
+end
+
+local function get_mode_from_props()
+    local regexdb = table.concat({shader_path, 'anime4k.csv'}, pathsep)
+    local f = csv.open(regexdb, {
+        separator = "\t",
+        columns = {
+            mode = {name = "MODE", transform = string.lower},
+            prop = {name = "PROP", transform = string.lower},
+            regex = {name = "REGEX"}
+        }
+    })
+    if f == nil then return nil end
+
+    for rule in f:lines() do
+        if not (rule.mode == nil or rule.prop == nil or rule.regex == nil) then
+            if match_prop(rule.prop, rule.regex) then
+                if validate_mode(rule.mode) then return rule.mode end
+            end
+        end
+    end
+    return nil
 end
 
 local function load_shader()
@@ -72,23 +100,17 @@ local function load_shader()
     local width = mp.get_property_native("width")
     if not height then return false end
     if not width then return false end
+    local mode = get_mode_from_props()
+
     msg.info("Res: " .. height .. "p")
-    if (height > 1080 or width > 1920) and o.uhd or
+    if o.autoload and ((height > 1080 or width > 1920) and o.uhd or
         (height <= 1080 and height >= 720 or width <= 1920 and width >= 1280) and
-        o.hd or height < 720 and width < 1280 then
-        load_shaders(o.mode)
+        o.hd or height < 720 and width < 1280) or not (mode == nil) then
+        load_shaders(mode == nil and o.mode or mode)
     else
-        unload_shaders()
+        load_shaders('none')
     end
     return true
-end
-
-local function set_mode(mode)
-    unload_shaders()
-    if validate_mode(mode) then
-        o.mode = mode
-        load_shader()
-    end
 end
 
 if not validate_mode(o.mode) then
@@ -99,10 +121,14 @@ end
 
 if o.enable then mp.register_event("file-loaded", load_shader) end
 
-mp.add_key_binding("CTRL+0", "anime4k-clear", unload_shaders)
-mp.add_key_binding("CTRL+1", "anime4k-load-a", function() set_mode('a') end)
-mp.add_key_binding("CTRL+2", "anime4k-load-b", function() set_mode('b') end)
-mp.add_key_binding("CTRL+3", "anime4k-load-c", function() set_mode('c') end)
-mp.add_key_binding("CTRL+4", "anime4k-load-aa", function() set_mode('aa') end)
-mp.add_key_binding("CTRL+5", "anime4k-load-bb", function() set_mode('bb') end)
-mp.add_key_binding("CTRL+6", "anime4k-load-ca", function() set_mode('ca') end)
+mp.add_key_binding("CTRL+0", "anime4k-clear",
+                   function() load_shaders('none') end)
+mp.add_key_binding("CTRL+1", "anime4k-load-a", function() load_shaders('a') end)
+mp.add_key_binding("CTRL+2", "anime4k-load-b", function() load_shaders('b') end)
+mp.add_key_binding("CTRL+3", "anime4k-load-c", function() load_shaders('c') end)
+mp.add_key_binding("CTRL+4", "anime4k-load-aa",
+                   function() load_shaders('aa') end)
+mp.add_key_binding("CTRL+5", "anime4k-load-bb",
+                   function() load_shaders('bb') end)
+mp.add_key_binding("CTRL+6", "anime4k-load-ca",
+                   function() load_shaders('ca') end)
