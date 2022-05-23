81f6a2fa7a858a8d5d26f070406cde71a2fd0fcc
Mubashshir <ahmubashshir@gmail.com>
2022-05-23 22:53:29 +0600

    zsh: Update propmt plugin
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- dot-config:zsh/plugins/devprompt/devprompt.plugin.zsh
+++ dot-config:zsh/plugins/devprompt/devprompt.plugin.zsh
@@ -12,14 +12,21 @@ DEVPROMPT[icon]='%F{blue}'
 DEVPROMPT[text]='%F{green}'
 DEVPROMPT[default]='%F{red}'
 
-add-zsh-hook precmd __DEVPROMPT_precmd
+add-zsh-hook precmd __devprompt_precmd
 
-function __DEVPROMPT_precmd {
+function __devprompt_env {
+	[[ -z $2 ]] && return 0
+
+	DEVPROMPT_PROMPT+="${DEVPROMPT[seperator]}${DEVPROMPT[prefix]}"
+	DEVPROMPT_PROMPT+="${DEVPROMPT[icon]}%B$1 ${DEVPROMPT[text]}$2%b"
+	DEVPROMPT_PROMPT+="${DEVPROMPT[default]}${DEVPROMPT[suffix]}"
+}
+
+function __devprompt_precmd {
+	emulate -L zsh
+	setopt hist_subst_pattern extendedglob
 	DEVPROMPT_PROMPT=''
-	[[ -n $VIRTUAL_ENV ]] && \
-		DEVPROMPT_PROMPT+="${DEVPROMPT[seperator]}${DEVPROMPT[prefix]}${DEVPROMPT[icon]}%B ${DEVPROMPT[text]}${VIRTUAL_ENV##*/}%b${DEVPROMPT[default]}${DEVPROMPT[suffix]}"
-	[[ -n $ROCK_ENV_NAME ]] && \
-		DEVPROMPT_PROMPT+="${DEVPROMPT[seperator]}${DEVPROMPT[prefix]}${DEVPROMPT[icon]}%B ${DEVPROMPT[text]}${ROCK_ENV_NAME}%b${DEVPROMPT[default]}${DEVPROMPT[suffix]}"
-	[[ -n $RBENV_VERSION ]] && \
-			DEVPROMPT_PROMPT+="${DEVPROMPT[seperator]}${DEVPROMPT[prefix]}${DEVPROMPT[icon]}%B ${DEVPROMPT[text]}${RBENV_VERSION}%b${DEVPROMPT[default]}${DEVPROMPT[suffix]}"
+	__devprompt_env  ${VIRTUAL_ENV:t:s@'%-[^-]##-py[0-9.]##'@@}
+	__devprompt_env  ${ROCK_ENV_NAME}
+	__devprompt_env  ${RBENV_VERSION}
 }
