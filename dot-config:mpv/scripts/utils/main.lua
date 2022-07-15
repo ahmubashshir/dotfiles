3b504a823720c78eea2e1037938b27e031cd5d0a
Mubashshir <ahmubashshir@gmail.com>
2022-07-15 19:04:20 +0600

    mpv: Set playback props based on video match
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- dot-config:mpv/scripts/utils/main.lua
+++ dot-config:mpv/scripts/utils/main.lua
@@ -1,7 +1,12 @@
 local opt = require("mp.options").read_options
-local o = {repl = true, quack = true}
+local msg = require "mp.msg"
 
+local o = {repl = true, quack = false, props = true}
 opt(o)
 
-if o.repl then require("repl") end
-if o.quack then require("quack") end
+for _mod, _load in pairs(o) do
+    if _load then
+        msg.info("Loading util." .. _mod)
+        require(_mod)
+    end
+end
