ce53901790ef17332c6d9c3e2d0ed8624f18b20f
Mubashshir <ahmubashshir@gmail.com>
2023-02-26 21:13:04 +0600

    env: Move env defs to shell config
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

---
+++ base/dot-config/shell/env.d/path.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+set -x
+export PATH="$(
+	mkpath () {
+		tr -s ':' '\n' \
+		| awk '!a[$0]++ {if(NR > 1) printf ":"; printf "%s", $0} END{print ""}'
+	}
+
+	cat <<EOF | mkpath
+$HOME/.local/bin
+$HOME/bin
+$XDG_DATA_HOME/cargo/bin
+$XDG_DATA_HOME/go/bin
+$PATH
+/usr/lib/surfraw
+$XDG_CONFIG_HOME/git/helpers
+$XDG_CONFIG_HOME/git/helpers/extras/bin
+EOF
+)"
+
+set +x
