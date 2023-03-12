42795a83939213373e36a7f379b3a69c0f315353
Mubashshir <ahmubashshir@gmail.com>
2023-03-12 22:23:30 +0600

    trackma/private: Handle error gracefully
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-config/trackma/hooks/private/__init__.py
+++ base/dot-config/trackma/hooks/private/__init__.py
@@ -5,6 +5,8 @@ from netrc import netrc
 
 
 def Creds(site: str = None):
-    login, account, secret = netrc().authenticators(f"trackma://{site}")
-
-    return (account or login, secret,)
+    try:
+       login, account, secret = netrc().authenticators(f"trackma://{site}")
+       return (account or login, secret,)
+    except:
+        return (None, None)
