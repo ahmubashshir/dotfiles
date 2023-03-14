d5f2e48c023c42e2546dce2234fcb1ebe636c3e4
Mubashshir <ahmubashshir@gmail.com>
2023-03-15 00:02:03 +0600

    bash: Fix environment
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-config/shell/env.d/common.sh
+++ base/dot-config/shell/env.d/common.sh
@@ -10,6 +10,6 @@ export GPGID=56DB0538F60D951C
 export GPGFP=916961EE198832DD70B628B356DB0538F60D951C
 export MEDIA_PLAYER=mpv-buffered
 export GPG_TTY="${TTY:-$(tty)}"
-if command -pv systemctl 2> /dev/null && systemctl is-active -q docker.socket && false;then
+if command -pv systemctl 2> /dev/null  >&2 && systemctl is-active -q docker.socket && false;then
 	export DOCKER_HOST="unix:///var$(systemctl cat docker.socket|awk -F = '/^ListenStream/{print $2;exit}')"
 fi
