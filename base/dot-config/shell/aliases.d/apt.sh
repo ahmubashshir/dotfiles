c95f63efc992ec6e438dd29eaf36024fad077a54
Mubashshir <ahmubashshir@gmail.com>
2023-03-31 00:12:55 +0600

    shell: Clean up aliases
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

---
+++ base/dot-config/shell/aliases.d/apt.sh
@@ -0,0 +1,8 @@
+alias apt-i="apt install"
+alias আপ্ত='apt'
+alias apt-r="apt remove"
+alias apt-ri="apt reinstall"
+alias apt-fu="apt full-upgrade"
+alias apt-du="apt dist-upgrade"
+alias apt-uu="apt upgrade"
+alias apt-u="apt update"
---
+++ base/dot-config/shell/aliases.d/dircolors.sh
@@ -0,0 +1,11 @@
+### Colored Output
+
+alias ip='ip -color=auto'
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias diff='diff --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias egrep='grep -E --color=auto'
+alias fgrep='fgrep --color=auto'
