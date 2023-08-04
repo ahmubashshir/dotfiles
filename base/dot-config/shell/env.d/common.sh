c7e37b15c99cc018d97c6910f1b74868ef3b8637
Mubashshir <ahmubashshir@gmail.com>
2023-08-04 20:46:41 +0600

    env: Check if systemctl is available before using it
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-config/shell/env.d/common.sh
+++ base/dot-config/shell/env.d/common.sh
@@ -10,8 +10,11 @@ export GPGID=56DB0538F60D951C
 export GPGFP=916961EE198832DD70B628B356DB0538F60D951C
 export MEDIA_PLAYER=mpv-buffered
 export GPG_TTY="${TTY:-$(tty)}"
-if command -pv systemctl 2> /dev/null  >&2 && systemctl is-active -q docker.socket;then
-	export DOCKER_HOST="unix:///var$(systemctl cat docker.socket|awk -F = '/^ListenStream/{print $2;exit}')"
-elif command -pv systemctl 2> /dev/null  >&2 && systemctl --user is-active -q podman.socket;then
-	export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-fi 2> /dev/null
+
+if command -pv systemct && systemctl is-system-running -q; then
+	if systemctl is-active -q docker.socket;then
+		export DOCKER_HOST="unix:///var$(systemctl cat docker.socket|awk -F = '/^ListenStream/{print $2;exit}')"
+	elif systemctl --user is-active -q podman.socket;then
+		export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+	fi
+fi 2> /dev/null >&2
