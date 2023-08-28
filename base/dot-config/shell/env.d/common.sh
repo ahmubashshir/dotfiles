f3a7477d873cc05bb9cd3df7151278698828f3a8
Mubashshir <ahmubashshir@gmail.com>
2023-08-28 12:42:11 +0600

    env: Default to podman
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-config/shell/env.d/common.sh
+++ base/dot-config/shell/env.d/common.sh
@@ -10,11 +10,8 @@ export GPGID=56DB0538F60D951C
 export GPGFP=916961EE198832DD70B628B356DB0538F60D951C
 export MEDIA_PLAYER=mpv-buffered
 export GPG_TTY="${TTY:-$(tty)}"
+export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
 
-if command -pv systemct && systemctl is-system-running -q; then
-	if systemctl is-active -q docker.socket;then
-		export DOCKER_HOST="unix:///var$(systemctl cat docker.socket|awk -F = '/^ListenStream/{print $2;exit}')"
-	elif systemctl --user is-active -q podman.socket;then
-		export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-	fi
+if command -pv systemctl && systemctl is-system-running -q && systemctl is-active -q docker.socket;then
+	export DOCKER_HOST="unix:///var$(systemctl cat docker.socket|awk -F = '/^ListenStream/{print $2;exit}')"
 fi 2> /dev/null >&2
