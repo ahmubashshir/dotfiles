b59c3146bf74fccd775e4b6319adf8870916707c
Mubashshir <ahmubashshir@gmail.com>
2024-11-02 09:32:34 +0600

    snap/rec: Rework organizer scripts
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/Videos/records/org.zsh
+++ base/Videos/records/org.zsh
@@ -2,7 +2,7 @@
 cd "${0:h}"
 setopt nullglob extendedglob rematchpcre nocaseglob hist_subst_pattern
 
-for each in **/*(ep|ova)-*.mp4;do
+for each in **/*(ep|ova)-*.*(^/);do
 	meta=${each:s+/+-+:gs/:/∶/}
 	meta=${each:s%-(#bi)(((ep(isode|)|ova)(-*|))-?ubbed)-%/${match[1]:l}/%}
 	name=${meta:h1}
@@ -12,13 +12,13 @@ for each in **/*(ep|ova)-*.mp4;do
 	mv "$each" "$name/${meta:h}/${meta:h2:t:r}.${meta:e}"
 done
 
-for each in **/*-*-*-*.mp4;do
+for each in **/*-*-*-*.*(^/);do
 	count=0
 	meta=${each:s+/+-+:gs/:/∶/}
-	meta=${each:s+(#bi)(*)-(*-*)-(*).mp4+'${match[1]}/${match[2]}/${match[3]}.mp4'+}
-	echo :: ${meta:h1}, ${meta:h2:t:r}, ${meta:e}
+	meta=${each:s+(#bi)(*)-(*-*)-(*).${meta:e}+'${match[1]}/${match[2]}/${match[3]}.${meta:e}'+}
+	echo :: ${meta:h1}, ${meta:h2:t}, ${meta:e}
 	mkdir -p "${meta:h1}"
-	mv "$each" "${meta:h1}/${meta:h2:t:r}.${meta:e}"
+	mv "$each" "${meta:h1}/${meta:h2:t}.${meta:e}"
 done
 
 find . -type d -empty -delete 2>/dev/null
