5cc613bf99073ad17db8f315d4f2996562bd8109
Mubashshir <ahmubashshir@gmail.com>
2024-11-24 16:19:50 +0600

    darkman: Change theme for lightmode
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-local/share/light-mode.d/xfce.sh
+++ base/dot-local/share/light-mode.d/xfce.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-sea
+xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-light-sea
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
 (sleep 1 && xfce4-panel --plugin-event wcktitle-5:refresh) &
