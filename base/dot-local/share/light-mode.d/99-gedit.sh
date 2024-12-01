c818894788c338d11b77b4a00040728c17f2879e
Mubashshir <ahmubashshir@gmail.com>
2024-12-01 20:41:21 +0600

    darkman/gedit: Fix gedit > 47
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-local/share/light-mode.d/99-gedit.sh
+++ base/dot-local/share/light-mode.d/99-gedit.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if ! gsettings describe \
-	'org.gnome.gedit.preferences.editor' \
-	'style-scheme-for-light-theme-variant' &> /dev/null
-then
-	gsettings set org.gnome.gedit.preferences.editor scheme '"icecream"'
+if ! gsettings describe 'org.gnome.gedit.preferences.ui' 'theme-variant' > /dev/null 2>&1; then
+	set -- org.gnome.gedit.preferences.editor scheme icecream
 else
-	gsettings set org.gnome.gedit.preferences.editor 'style-scheme-for-light-theme-variant' '"icecream"'
+	set -- org.gnome.gedit.preferences.ui theme-variant light
 fi
+
+exec gsettings set "$@"
