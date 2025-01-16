a838f4b9b6d91f72a1aab2fbf24cb4169949fd57
Mubashshir <ahmubashshir@gmail.com>
2025-01-16 14:58:55 +0600

    darkman: Change Cursor depending on mode
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-local/share/dark-mode.d/00-xfce.sh
+++ base/dot-local/share/dark-mode.d/00-xfce.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-dark-sea
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
+xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Fluent-dark-cursors
 sh -c 'sleep 1 && xfce4-panel --plugin-event wcktitle-5:refresh' &
--- base/dot-local/share/light-mode.d/00-xfce.sh
+++ base/dot-local/share/light-mode.d/00-xfce.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-light-sea
+xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-sea
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
+xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Fluent-cursors
 sh -c 'sleep 1 && xfce4-panel --plugin-event wcktitle-5:refresh' &
