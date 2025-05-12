aaa6180dc84d4a12387d5b2b73b02585b6ebe8c6
Mubashshir <ahmubashshir@gmail.com>
2025-05-12 22:27:18 +0600

    xfdeskto[: Add automated dark/light switching
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-local/share/light-mode.d/00-xfce.sh
+++ base/dot-local/share/light-mode.d/00-xfce.sh
@@ -2,4 +2,6 @@
 xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-light-sea
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
 xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Fluent-cursors
+! test -f ~/.config/backdrops/day || xfce4-setbg ~/.config/backdrops/day
+
 sh -c 'sleep 1 && xfce4-panel --plugin-event wcktitle-5:refresh' &
