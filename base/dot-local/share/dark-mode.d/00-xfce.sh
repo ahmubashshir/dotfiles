5e639dfb9f959a5bec5350d1a10a4aa4457e1502
Mubashshir <ahmubashshir@gmail.com>
2026-01-01 04:14:17 +0600

    xfce: Fix mode switch
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- base/dot-local/share/dark-mode.d/00-xfce.sh
+++ base/dot-local/share/dark-mode.d/00-xfce.sh
@@ -2,6 +2,6 @@
 xfconf-query -c xsettings -p /Net/ThemeName     -s Matcha-dark-sea
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
 xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Fluent-dark-cursors
-! test -f ~/.config/backdrops/night || xfce4-setbg ~/.config/backdrops/night
+! test -f ~/.config/backdrops/night || ~/bin/xfce4-setbg ~/.config/backdrops/night
 
-sh -c 'sleep 1 && xfce4-panel --plugin-event wcktitle-5:refresh' &
+~/bin/xfce4-wck-refresh &!
