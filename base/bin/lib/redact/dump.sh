17003dd5157fc9837e1ec27f3ded6ec4c83603e1
Mubashshir <ahmubashshir@gmail.com>
2026-07-23 00:19:03 +0600

    base/bin/redact: Move help and dump code to separate modules
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

---
+++ base/bin/lib/redact/dump.sh
@@ -0,0 +1,23 @@
+ARGSPEC['dump']='@'
+SHORT['d']='dump'
+
+enable-dump()
+{
+	set-enabled dump-rules || return
+}
+
+helptext-dump()
+{
+	cat << EOF
+  -d, --dump
+               dump generated replacement rules
+EOF
+}
+
+printRules() {
+	printf '%%RULES {\n'
+	printf '\t%s\n' "${RULES[@]}"
+	printf '}\n\nsed -uEf %%RULES --'
+	((${#args[@]} == 0)) || printf ' %Q' "${args[@]}"
+	printf '\n'
+} >&2
---
+++ base/bin/lib/redact/help.sh
@@ -0,0 +1,29 @@
+ARGSPEC['help']='@'
+SHORT['h']='help'
+
+enable-help()
+{
+	set-enabled show-help || return
+}
+
+printHelp()
+{
+	cat << 'EOF'
+Usage: redact [option]... @[ENV]... [--] [file]... < [file]... > output
+Scrub local environment information from input stream
+
+Options:
+EOF
+
+	for mode in "${IMMEDIATES[@]}" "${GENERATORS[@]}"; do
+		declare -F "helptext-$mode" > /dev/null || continue
+
+		"helptext-$mode"
+	done <&-
+
+	cat << 'EOF'
+  -h, --help
+               show this help text
+EOF
+	exit 0
+} >&2
